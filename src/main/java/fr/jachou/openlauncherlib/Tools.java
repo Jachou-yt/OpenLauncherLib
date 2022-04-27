40c3f72541618dc49771d0a101105829323a3583
Jachou <84678307+Jachou-yt@users.noreply.github.com>
2022-04-27 14:22:37 +0200

    Add Tools.java for a future youtube tutorial.

---
+++ src/main/java/fr/jachou/openlauncherlib/Tools.java
@@ -0,0 +1,42 @@
+package fr.jachou.openlauncherlib;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+
+public class Tools {
+    public static BufferedImage getBufferedImage(String fileName, Class mainClass) throws IOException {
+        InputStream inputStream = mainClass.getClassLoader().getResourceAsStream(fileName);
+        return ImageIO.read(inputStream);
+    }
+
+    public static Image getImage(File fileName, Class mainClass) throws IOException {
+        InputStream inputStream = mainClass.getClassLoader().getResourceAsStream(String.valueOf(fileName));
+        return ImageIO.read(inputStream);
+    }
+
+    public static void CreateFile(File file) throws IOException {
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+    }
+
+    public static void OpenUrl(String url) throws IOException {
+        Desktop.getDesktop().browse(URI.create(url));
+    }
+
+    public static int getPlayers(String serverIp) throws IOException {
+        int joueur = 0;
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(new URL(serverIp).openStream()));
+        joueur = in.toString().length();
+
+        return joueur;
+    }
+
+
+
+}
